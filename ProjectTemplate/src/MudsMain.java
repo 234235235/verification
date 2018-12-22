9091d21b9fffc5318af7c6a7620e2f08a3b22d45
Christian Becker <s9crbec2@stud.uni-saarland.de>
2018-12-22 12:16:52 +0100

    part2

--- ProjectTemplate/src/MudsMain.java
+++ ProjectTemplate/src/MudsMain.java
@@ -1,29 +1,15 @@
-import java.util.concurrent.TimeUnit;
+
 
 public class MudsMain {
 
 	
 	public static void main(String[] args) {
 		
-		AbstractChecker checker = new Part1();
-		
-		for (String arg : args) {
-			System.out.println("Running: "+arg);
-			checker.run(arg);
-			try {
-				TimeUnit.MILLISECONDS.sleep(10);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("########################################");
-		}
-		
-		/*
+		AbstractChecker checker = new Part2();
 		if( args.length == 1)
 			checker.run(args[0]);
 		else
-			System.err.println("Provide a filename as first argument."); */
+			System.err.println("Provide a filename as first argument.");
 	}
 }
 
